f9002d65359b7cb03b1e46631c96570e8c2f9838
marinsfabio <fabiomarinspro@hotmail.com>
2023-09-11 22:42:22 -0300

    primeiros passos

---
+++ app/src/main/java/com/example/motivation/UserActivity.kt
@@ -0,0 +1,44 @@
+package com.example.motivation
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityUserBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+        val name = binding.editName.text.toString()
+        if(name != "") {
+            //  direcionando pra outra activity
+            startActivity(
+                Intent(
+                    this,
+                    MainActivity::class.java))
+        } else {
+            Toast.makeText(
+                this,
+                R.string.validation_mandatory_name,
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
